8fad83374c9b23bccb1a2cd0bacd95f38c098f73
Georg Teufelberger <tgeorg@student.ethz.ch>
2024-10-29 16:24:57 +0100

    docs: Add comment

--- src/scripts/spotify-to-csv.py
+++ src/scripts/spotify-to-csv.py
@@ -235,7 +235,7 @@ def calculate_start_times_aligned(df, start_time):
 
     for index, row in df.iterrows():
         print(f"{index=}")
-        if index == 14:
+        if index == 14: # Add extra time for a break, e.g. an announcement
             current_time += timedelta(milliseconds=5 * 60 * 1000)
 
         # Always print the first and last song's start time
