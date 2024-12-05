4f745ea4bb8159b9b437a82247eeb0febfc0b66f
Georg Teufelberger <tgeorg@student.ethz.ch>
2024-12-05 14:15:45 +0100

    feat: Add support for transitioning to outro

--- src/scripts/spotify-to-csv.py
+++ src/scripts/spotify-to-csv.py
@@ -425,31 +425,38 @@ class DanceStateMachine:
         self.add_transition(
             "Salsa",
             "Bachata",
+            "Outro",
         )
         self.add_transition(
             "Bachata",
             "English Waltz",
+            "Outro",
         )
         self.add_transition(
             "English Waltz",
             "English Tango",
+            "Outro",
         )
         self.add_transition(
             "English Tango",
             "Viennese Waltz",
+            "Outro",
         )
         self.add_transition(
             "Viennese Waltz",
             "Slow Fox",
             "Quickstep",
+            "Outro",
         )
         self.add_transition(
             "Slow Fox",
             "Quickstep",
+            "Outro",
         )
         self.add_transition(
             "Quickstep",
             "Discofox",
+            "Outro",
         )
         self.add_transition(
             "Discofox",
@@ -457,33 +464,40 @@ class DanceStateMachine:
             "Charleston",
             "Samba",
             "Cha Cha Cha",
+            "Outro",
         )
         self.add_transition(
             "Lindy Hop",
             "Samba",
             "Cha Cha Cha",
+            "Outro",
         )
         self.add_transition(
             "Charleston",
             "Samba",
             "Cha Cha Cha",
+            "Outro",
         )
         self.add_transition(
             "Samba",
             "Cha Cha Cha",
+            "Outro",
         )
         self.add_transition(
             "Cha Cha Cha",
             "Rumba",
+            "Outro",
         )
         self.add_transition(
             "Rumba",
             "Paso Doble",
             "Jive",
+            "Outro",
         )
         self.add_transition(
             "Paso Doble",
             "Jive",
+            "Outro",
         )
         self.add_transition(
             "Jive",
@@ -492,6 +506,7 @@ class DanceStateMachine:
             "Polka",
             "Salsa",
             "Choreo",
+            "Outro",
         )
         self.add_transition(
             "Tango Argentino",
@@ -504,6 +519,7 @@ class DanceStateMachine:
             "Kizomba / Zouk",
             "Polka",
             "Salsa",
+            "Outro",
         )
         self.add_transition(
             "Polka",
